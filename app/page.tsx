e24ebdadcdd6dcfa5a81b715d84c6e1679333851
Miles Whiticker <miles.whiticker@gmail.com>
2025-05-18 20:52:32 +1000

    feat: add user authentication
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-"use client";
+"use client"
 
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const { signOut } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -34,10 +32,11 @@ export default function App() {
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
   }
+  
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+          <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} 
@@ -45,14 +44,17 @@ export default function App() {
 			{todo.content}
 		</li>
         ))}
-      </ul>
+          </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
-      </div>
+          </div>
+          <br />
+
+          <button onClick={signOut}>Sign out</button>         
     </main>
   );
 }
